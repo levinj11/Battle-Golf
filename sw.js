7acfeca1aeeb728df4d7bc50b7a623b450e8284d
levinj11 <141527876+levinj11@users.noreply.github.com>
2026-03-26 12:11:52 -0700

    Update offline fallback page to 'Battle Golf.html'

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox
 const CACHE = "pwabuilder-page";
 
 // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = "ToDo-replace-this-name.html";
+const offlineFallbackPage = "Battle Golf.html";
 
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
@@ -39,7 +39,7 @@ self.addEventListener('fetch', (event) => {
       } catch (error) {
 
         const cache = await caches.open(CACHE);
-        const cachedResp = await cache.match(offlineFallbackPage);
+        const cachedResp = await cache.match(Battle Golf.html);
         return cachedResp;
       }
     })());
